22ba6585ec66d144362eb8987f2c90a8022a3aa4
CobyBoy <aboutacoby@gmail.com>
2022-03-20 23:15:00 -0300

    Added sum and sorting of powerstats and formatting

--- src/app/team-container/team-container.component.ts
+++ src/app/team-container/team-container.component.ts
@@ -3,7 +3,6 @@ import { TeamContainerService } from './services/team-container.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddHeroComponent } from '../add-hero/add-hero.component';
 import { Hero } from '../add-hero/hero';
-import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-team-container',
@@ -12,12 +11,13 @@ import { tap } from 'rxjs';
 })
 export class TeamContainerComponent implements OnInit {
   heroListInContainer: Hero[] = [];
-  combatSum: number = 0;
-  durabilitySum: number = 0;
-  intelligenceSum: number = 0;
-  powerSum: number = 0;
-  speedSum: number = 0;
-  strengthSum: number = 0;
+  combatSum!: number;
+  durabilitySum!: number;
+  intelligenceSum!: number;
+  powerSum!: number;
+  speedSum!: number;
+  strengthSum!: number;
+  powerStatsSortedFormatted!: string[];
 
   constructor(
     private teamService: TeamContainerService,
@@ -27,21 +27,62 @@ export class TeamContainerComponent implements OnInit {
   ngOnInit(): void {
     this.teamService.currentHero.subscribe((heroes: Hero[]) => {
       this.heroListInContainer = [...heroes];
-      heroes.forEach((hero: Hero) => {
-        this.combatSum += ~~hero.powerstats.combat;
-        this.durabilitySum += Number(hero.powerstats.durability);
-        this.intelligenceSum += Number(hero.powerstats.intelligence);
-        this.powerSum += Number(hero.powerstats.power);
-        this.speedSum += Number(hero.powerstats.speed);
-        this.strengthSum += Number(hero.powerstats.strength);
-      });
       console.log('received hero subscribe ngoinit', heroes);
       console.log('oninit onsubscribe list', this.heroListInContainer);
+      this.calculatePowerStats();
     });
-    
   }
 
   openDialog(): void {
     this.dialog.open(AddHeroComponent);
+  }
+
+  private calculatePowerStats(): void {
+    console.log('calculating...');
+    this.combatSum = 0;
+    this.durabilitySum = 0;
+    this.intelligenceSum = 0;
+    this.powerSum = 0;
+    this.speedSum = 0;
+    this.strengthSum = 0;
+    this.heroListInContainer.forEach((hero: Hero) => {
+      let powerstats = hero.powerstats;
+      this.combatSum += ~~powerstats.combat;
+      this.durabilitySum += ~~powerstats.durability;
+      this.intelligenceSum += ~~powerstats.intelligence;
+      this.powerSum += ~~powerstats.power;
+      this.speedSum += ~~powerstats.speed;
+      this.strengthSum += ~~powerstats.strength;
+    });
+    this.calculateMax();
+  }
+
+  private calculateMax(): void {
+    let powerstats = {
+      combat: this.combatSum,
+      durability: this.durabilitySum,
+      intelligence: this.intelligenceSum,
+      power: this.powerSum,
+      speed: this.speedSum,
+      strength: this.strengthSum,
+    };
+
+    let powerStatsKeyAndValue = Object.entries(powerstats);
+    //sort it by max value
+    let powerStatsSorted = powerStatsKeyAndValue.sort(
+      (a: [string, number], b: [string, number]) => b[1] - a[1]
+    );
+    console.log('entriesSorted', powerStatsSorted);
+    this.formatPowerStats(powerStatsSorted);
+  }
+
+  //formats it so it shows combat: 0 instead of combat,0
+  private formatPowerStats(powerStatsSorted: [string, number][]): void {
+    this.powerStatsSortedFormatted = powerStatsSorted.map(
+      (entry: [string, number]) => {
+        return entry.join().replace(',', ': ');
+      }
+    );
+    console.log(powerStatsSorted);
   }
 }
